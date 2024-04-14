50cc17f54739f5babda376a0cb10ebdf197fbc97
Veniamin <bm0651@yandex.ru>
2024-04-14 20:59:11 +0300

    сделал интерфейс закрузки для сотрудников

--- gtrkstaff/src/views/uploadEmployee/index.js
+++ gtrkstaff/src/views/uploadEmployee/index.js
@@ -1,9 +1,52 @@
 import React  from "react";
 import Container from "react-bootstrap/esm/Container";
 import MenuUser from "../../components/menuUser";
-import {Form, Col, Row} from 'react-bootstrap/'
+import {Form, Col, Row, Button} from 'react-bootstrap/';
+import Modal from 'react-bootstrap/Modal';
 import './style.css'
 class UploadEmployee extends React.Component{
+  constructor(props){
+    super(props)
+    
+    this.handleClose = this.handleClose.bind(this);
+    //this.handleCheck = this.handleCheck.bind(this);
+    this.state={
+        show: false,
+        sucsess: false,
+        filename: String
+    }
+
+    this.selectFile = this.selectFile.bind(this);
+    this.fileUpload = this.fileUpload.bind(this);
+}
+
+    selectFile(evn){
+      this.setState({filename: evn.target.files});
+    }
+    handleClose(){
+      this.setState({show:false})
+    }
+
+fileUpload(evn){
+  evn.preventDefault();
+  const fData = new FormData();
+  fData.append('file', this.state.filename);
+  let url = `${document.location.protocol}/uploadempl`;
+  fetch(url, {
+    method: 'post',
+    body: fData
+  }).then(res =>{
+    if(res.ok){
+      this.setState({sucsess:true, show:true});
+    }else{
+      this.setState({sucsess:false, show:true});
+    }
+  }).catch(ex=>{
+    this.setState({sucsess:false, show:true});
+  })
+
+}
+
     render() {
         return (
         <Container>
@@ -13,10 +56,52 @@ class UploadEmployee extends React.Component{
           <Col></Col>
           <Form.Group controlId="formFileLg" className="mb-3 control50">
                 <Form.Label>Загрузить Эксель файл </Form.Label>
-                <Form.Control type="file" size="lg" />
+                <Form.Control onChange={this.selectFile} type="file" size="lg" />
           </Form.Group>
-          <Col></Col>
+          <Col>
+            
+          </Col>
           </Row>
+          <Row>
+            <Col xs={8}></Col> <Col><Button onClick={this.fileUpload}>Загрузить </Button></Col>
+          </Row>
+          {this.state.sucsess? 
+          <Modal show={this.state.show }
+              size="lg"
+              aria-labelledby="contained-modal-title-vcenter"
+              centered
+            >
+            <Modal.Header closeButton>
+              <Modal.Title id="contained-modal-title-vcenter">
+                  Modal heading
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <h4>Результат загрузки!</h4>
+              <p>  download        </p>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={this.handleClose}>Close</Button>
+            </Modal.Footer>
+          </Modal> :
+          <Modal show={this.state.show }
+              size="lg"
+              aria-labelledby="contained-modal-title-vcenter"
+              centered
+            >
+              <Modal.Header closeButton>
+                  <Modal.Title id="contained-modal-title-vcenter">
+                    Загрузить не удалось
+                  </Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                  <h4>Ошибка загрузки!!</h4>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button onClick={this.handleClose}>Закрыть</Button>
+              </Modal.Footer>
+          </Modal>}
+
         </Container>)
       }
 }
